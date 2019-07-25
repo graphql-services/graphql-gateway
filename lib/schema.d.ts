1866f5ee6afaf5fadef819db2ec8486e77cceabf
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-07-25 07:16:55 +0200

    Add support for GraphQLError

--- lib/schema.d.ts
+++ lib/schema.d.ts
@@ -1,1 +1,2 @@
-export declare const getSchemaFromURLS: (urls: string[]) => Promise<any>;
+import { GraphQLSchema } from 'graphql';
+export declare const getSchemaFromURLS: (urls: string[]) => Promise<GraphQLSchema | null>;
--- src/schema.ts
+++ src/schema.ts
@@ -1,13 +1,15 @@
-import { GraphQLSchema } from 'graphql';
-import fetch from 'node-fetch';
-import { HttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
+import { GraphQLError, GraphQLSchema } from 'graphql';
 import {
-  mergeSchemas,
+  introspectSchema,
   makeRemoteExecutableSchema,
-  introspectSchema
+  mergeSchemas
 } from 'graphql-tools';
+
+import { HttpLink } from 'apollo-link-http';
+import fetch from 'node-fetch';
 import { getENV } from './env';
+import { onError } from 'apollo-link-error';
+import { setContext } from 'apollo-link-context';
 
 const forwardedHeaders = getENV(
   'GRAPHQL_FORWARD_HEADERS',
@@ -27,7 +29,7 @@ export const getSchemaFromURLS = async (
 const getRemoteSchema = async (url: string): Promise<GraphQLSchema> => {
   const http = new HttpLink({ uri: url, fetch });
 
-  const link = setContext((request, previousContext) => {
+  let link = setContext((request, previousContext) => {
     const req =
       previousContext.graphqlContext && previousContext.graphqlContext.req;
 
@@ -42,6 +44,26 @@ const getRemoteSchema = async (url: string): Promise<GraphQLSchema> => {
       headers
     };
   }).concat(http);
+
+  link = onError(({ response }) => {
+    if (typeof response === 'undefined') {
+      return;
+    }
+    response.errors =
+      response.errors &&
+      response.errors.map(
+        err =>
+          new GraphQLError(
+            err.message,
+            undefined,
+            null,
+            err.locations && err.locations.map(loc => loc.line),
+            err.path,
+            null,
+            err.extensions
+          )
+      );
+  }).concat(link);
 
   const schema = await introspectSchema(link);
 
