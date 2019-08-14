8c060cfc5338c74371ef481974c291ae2d90e8d8
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-08-14 15:06:53 +0200

    Remove unnecessary code

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-try {
-  require('dotenv').config();
-} catch (err) {}
+const { getENVArray } = require('./env');
 
-const { start } = require('./lib/app');
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway, RemoteGraphQLDataSource } = require('@apollo/gateway');
 
-start().catch(err => {
-  console.log('failed to start:', err);
+const urls = getENVArray('GRAPHQL_URL');
+const names = getENVArray('GRAPHQL_NAME', []);
+
+const gateway = new ApolloGateway({
+  serviceList: urls.map((x, i) => ({ name: names[i] || x, url: x })),
+  // [
+  //   // { name: "accounts", url: "http://localhost:4001/graphql" },
+  //   { name: 'orm', url: 'http://localhost:8080/graphql' },
+  //   { name: 'reviews', url: 'http://localhost:4002/' }
+  //   // { name: 'products', url: 'http://localhost:4003/graphql' }
+  //   // { name: "inventory", url: "http://localhost:4004/graphql" }
+  // ],
+  buildService({ name, url }) {
+    return new RemoteGraphQLDataSource({
+      url,
+      willSendRequest({ request, context }) {
+        request.http.headers.set('x-correlation-id', '...');
+        console.log('will send request -> ', name, JSON.stringify(request));
+      }
+    });
+  }
 });
+
+(async () => {
+  const { schema, executor } = await gateway.load();
+
+  const server = new ApolloServer({ schema, executor });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+})();
