e5715eeaf288372f55add7b9c817eb3868f1e6a6
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-08-19 05:23:18 +0200

    Add authorization header forwarding

--- index.js
+++ index.js
@@ -8,18 +8,15 @@ const names = getENVArray('GRAPHQL_NAME', []);
 
 const gateway = new ApolloGateway({
   serviceList: urls.map((x, i) => ({ name: names[i] || x, url: x })),
-  // [
-  //   // { name: "accounts", url: "http://localhost:4001/graphql" },
-  //   { name: 'orm', url: 'http://localhost:8080/graphql' },
-  //   { name: 'reviews', url: 'http://localhost:4002/' }
-  //   // { name: 'products', url: 'http://localhost:4003/graphql' }
-  //   // { name: "inventory", url: "http://localhost:4004/graphql" }
-  // ],
   buildService({ name, url }) {
     return new RemoteGraphQLDataSource({
       url,
       willSendRequest({ request, context }) {
-        request.http.headers.set('x-correlation-id', '...');
+        // request.http.headers.set('x-correlation-id', '...');
+        request.http.headers.set(
+          'authorization',
+          context.req.headers['authorization']
+        );
         console.log('will send request -> ', name, JSON.stringify(request));
       }
     });
@@ -29,7 +26,11 @@ const gateway = new ApolloGateway({
 (async () => {
   const { schema, executor } = await gateway.load();
 
-  const server = new ApolloServer({ schema, executor });
+  const server = new ApolloServer({
+    schema,
+    executor,
+    context: ({ req }) => ({ req })
+  });
   const PORT = getENV('PORT', '80');
 
   server.listen({ port: PORT }).then(({ url }) => {
