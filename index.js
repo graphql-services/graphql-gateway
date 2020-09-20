33601ca5670f65ec278c47b1c96390ba5ae45b28
Hans Erik Heggem <hans.erik.heggem@gmail.com>
2020-09-20 22:57:20 +0200

    Implemented graphql gateway update interval

--- index.js
+++ index.js
@@ -4,18 +4,30 @@ const { getApolloServer } = require("./apollo");
 
 (async () => {
   const app = express();
-  app.use(express.json({ limit: "2mb" }));
+  const jsonBodyLimit = getENV("GRAPHQL_JSON_BODY_LIMIT", "2mb");
+  app.use(express.json({ limit: jsonBodyLimit }));
 
   const server = await getApolloServer();
   server.applyMiddleware({ app });
 
+  const activateUpdateGatewayInterval = getENV("GRAPHQL_UPDATE_GATEWAY", "false") === "true";
+  const updateGatewayInterval = getENV("GRAPHQL_UPDATE_GATEWAY_INTERVAL_MS", "60000");
+  if (activateUpdateGatewayInterval === true)
+  {
+    setInterval(async () => {
+      const server = await getApolloServer();
+      server.applyMiddleware({ app });
+    }, updateGatewayInterval);
+  }
+
+  const HOST = getENV("HOST", "http://localhost");
   const PORT = getENV("PORT", "80");
 
   // app.listen({ port: PORT }).then(({ url }) => {
   //   console.log(`🚀 Server ready at ${url}`);
   // });
   app.listen({ port: PORT }, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+    console.log(`🚀 Server ready at ${HOST}:${PORT}/graphql`);
   });
 })();
 
--- lambda.js
+++ lambda.js
@@ -10,7 +10,19 @@ const getServer = async () => {
   if (!server) {
     const apolloServer = await getApolloServer(true);
     const app = express();
-    app.use(express.json({ limit: "2mb" }));
+    const jsonBodyLimit = getENV("GRAPHQL_JSON_BODY_LIMIT", "2mb");
+    app.use(express.json({ limit: jsonBodyLimit }));
+
+    const activateUpdateGatewayInterval = getENV("GRAPHQL_UPDATE_GATEWAY", "false") === "true";
+    const updateGatewayInterval = getENV("GRAPHQL_UPDATE_GATEWAY_INTERVAL_MS", "60000");
+    if (activateUpdateGatewayInterval === true)
+    {
+      setInterval(async () => {
+        const server = await getApolloServer(true);
+        server.applyMiddleware({ app });
+      }, updateGatewayInterval);
+    }
+
     apolloServer.applyMiddleware({ app });
 
     server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
